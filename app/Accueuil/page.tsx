f06a9117506be28931bf1114dc1741b115bd0e81
Gael-dev226 <gaelnkm0@gmail.com>
2026-03-06 07:50:44 +0000

    Update page.tsx

--- app/Accueuil/page.tsx
+++ app/Accueuil/page.tsx
@@ -1,16 +1,9 @@
-import Hero from '@/components/hero'
+
 import React from 'react'
-import CardInfo from '@/components/cardInfo'
 const page = () => {
     return (
         <div>
-            <Hero />
-
-            <div className="p-10">
-
-                <CardInfo />
-
-            </div>
+            
         </div>
     )
 }
